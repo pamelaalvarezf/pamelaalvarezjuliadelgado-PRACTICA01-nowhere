03b1da7a9812b9cfdd6493ab0dbb25847d079c7b
JuliaDMB <julitadmb@gmail.com>
2024-10-26 15:27:02 +0200

    add instagram add modalcatalog add arrow add picadd typography

--- JS/script-catalog.js
+++ JS/script-catalog.js
@@ -1,13 +1,3 @@
-var typed = new Typed("#typed-text", {
-    strings: [
-        "Some typed text",
-        "Created with TypeJS library",
-        "Some other text",
-        "The gallery was created with CSS Grid, check it out!",
-    ],
-    typeSpeed: 80,
-});
-
 ScrollReveal().reveal(".grid-wrapper > div", {
 delay: 200,
 distance: "50px",
