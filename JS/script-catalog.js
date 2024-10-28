388b170312080997ec9e69913d832471c7c50cde
pamelaalvarezf <pamelaalvarezfuentes@gmail.com>
2024-10-28 17:18:41 +0100

    prueba

--- JS/script-catalog.js
+++ JS/script-catalog.js
@@ -5,3 +5,19 @@ interval: 100,
 origin: "bottom",
 scale: 0.9,
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fadeElements = document.querySelectorAll('.fade-in');
+    
+    const handleScroll = () => {
+        fadeElements.forEach(el => {
+            const rect = el.getBoundingClientRect();
+            if (rect.top < window.innerHeight - 100) {
+                el.classList.add('visible');
+            }
+        });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+});
